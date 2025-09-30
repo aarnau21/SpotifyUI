f2157c66990d55be952fe8521ba884145e4003d2
aarnau21 <aarnau@unc.edu>
2025-09-30 19:32:07 -0400

    Test push

---
+++ SpotifyUI/SpotifyUI/SpotifyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyUIApp.swift
+//  SpotifyUI
+//
+//  Created by Aarna Upadhyaya on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpotifyUI/SpotifyUITests/SpotifyUITests.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyUITests.swift
+//  SpotifyUITests
+//
+//  Created by Aarna Upadhyaya on 9/30/25.
+//
+
+import Testing
+@testable import SpotifyUI
+
+struct SpotifyUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
